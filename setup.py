c724d604499a89cbd5877e926cec17a550a42296
marquez85 <fpvsm85@gmail.com>
2022-08-25 01:04:02 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,7 @@ setup(
    description='essencial py files to run to connect plaxis',
    author='PLAXIS_FM',
    author_email='fpvsm85@gmail.com',
-   py_modules=['encryption.py'],
-   url="https://github.com/marquez85/encryption",
-   install_requires=[],
+   py_modules=['encryption'],
    entry_points={
        'console_script':[
            'encryption=encryption',
